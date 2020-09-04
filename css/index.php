96301804ab0b9ba2ed65aabb6f4b99338b3b4fb8
ReaNaegeli <rea.naegeli@schulemb.ch>
2020-09-04 11:13:04 +0200

    Task 28, Teilaufgabe e

---
+++ css/index.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>
+      Rea's erste Website
+    </title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+
+  </head>
+
+  <body>
+
+        7 + 6 = 7<br>
+<p>
+        <?php
+            $zahl1 = 7;
+            $zahl2 = 6;
+            $ergebnis = $zahl1 + $zahl2;
+            echo "Ergebnis: $ergebnis";
+        ?>
+    </body>
+</html>
+
+
+
